e69a876d27133de5437c5a8645eb620e54f75e7b
YoussefIslam217 <youssef23012591@gmail.com>
2026-02-28 22:23:02 +0200

    Sixth Commit

--- Recursion-Problems/Recursion-Problems.cpp
+++ Recursion-Problems/Recursion-Problems.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
-
+//Problem-F. Print Even Indices
+void PrintEvenIndices(int arr[], int ArraySize, int n)
+{
+	if (n >= ArraySize)
+		return;
+	cout << arr[n] << " ";
+	PrintEvenIndices(arr, ArraySize, n + 2);
+}
 int main()
 {
-    cout << "Ramadan Kareem, Eng.Mahmoud Hammad";
+	int ArraySize, n = 0;
+	cout << "Enter the size of the array: ";
+	cin >> ArraySize;
+	int* arr = new int[ArraySize];
+	cout << "Enter the elements of the array: ";
+	for (int i = 0; i < ArraySize; i++)
+		cin >> arr[i];
+	cout << "The elements at even indices are: ";
+	PrintEvenIndices(arr, ArraySize, n);
+	delete[] arr;
+	return 0;
 }
-
 
