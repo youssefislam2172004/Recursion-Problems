49d0e339771cf4a53a275ea70de638a949c46fdc
YoussefIslam217 <youssef23012591@gmail.com>
2026-02-28 22:33:56 +0200

    Ninth Commit

--- Recursion-Problems/Recursion-Problems.cpp
+++ Recursion-Problems/Recursion-Problems.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
+#include <string>
 using namespace std;
+//I. Count Vowels
+int countVowels(string str, char vowels[], int i)
+{
+	if (i == str.size())
+		return 0;
+	int count = 0;
+	for (int j = 0; j < 5; j++)
+	{
+		if (tolower(str[i]) == vowels[j])
+			count++;
+	}
+	return count + countVowels(str, vowels, i + 1);
+}
 
 int main()
 {
-    cout << "Ramadan Kareem, Eng.Mahmoud Hammad";
+	string str;
+	char vowels[] = { 'a', 'e', 'i', 'o', 'u' };
+	cout << "Enter a string: ";
+	getline(cin, str);
+	int count = countVowels(str, vowels, 0);
+	cout << "Number of vowels in the string: " << count << endl;
+	return 0;
 }
-
 
