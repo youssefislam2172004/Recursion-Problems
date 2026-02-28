11f6693db580e609f990cfe75edd62949238318b
YoussefIslam217 <youssef23012591@gmail.com>
2026-02-28 22:36:59 +0200

    Tenth Commit

--- Recursion-Problems/Recursion-Problems.cpp
+++ Recursion-Problems/Recursion-Problems.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
+//J. Factorial
+int Factorial(int n)
+{
+	if (n == 1 || n == 0)
+		return 1;
+	else
+		return n * Factorial(n - 1);
+}
 
 int main()
 {
-    cout << "Ramadan Kareem, Eng.Mahmoud Hammad";
+	int number;
+	cout << "Enter a number: ";
+	cin >> number;
+	cout << "Factorial of " << number << " is: " << Factorial(number) << endl;
 }
-
 
