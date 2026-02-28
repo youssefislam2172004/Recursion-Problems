9354cb41f2ba6c9ae875e20c30a20b2957b388a8
YoussefIslam217 <youssef23012591@gmail.com>
2026-02-28 22:30:02 +0200

    Eighth Commit

--- Recursion-Problems/Recursion-Problems.cpp
+++ Recursion-Problems/Recursion-Problems.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
+//H. Inverted Pyramid
+void InvertedPyramid(int w, int n)
+{
+	if (n == 0)
+		return;
+	InvertedPyramid(w + 2, n - 1);
+	for (int i = 0; i < n; i++)
+		cout << " ";
+	for (int i = 0; i < w; i++)
+		cout << "*";
+	cout << endl;
+}
 
 int main()
 {
-    cout << "Ramadan Kareem, Eng.Mahmoud Hammad";
+	int width = 1, numberOfLines;
+	cout << "Enter the number of lines: ";
+	cin >> numberOfLines;
+	InvertedPyramid(width, numberOfLines);
+	return 0;
 }
-
 
