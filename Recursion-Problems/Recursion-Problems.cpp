067560f282238b2fa6cc14c02414a6e5c112ff19
YoussefIslam217 <youssef23012591@gmail.com>
2026-02-28 21:55:58 +0200

    First-Commit

--- Recursion-Problems/Recursion-Problems.cpp
+++ Recursion-Problems/Recursion-Problems.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
+//Problem-A. Print Recursion
+
+void Print(int n)
+{
+    if (n == 0)
+        return;
+    else
+        cout << "I love Recursion" << endl;
+    Print(n - 1);
+}
 
 int main()
 {
-    cout << "Ramadan Kareem, Eng.Mahmoud Hammad";
+    int numbers;
+    cout << "Enter the number of times you want to print 'I love Recursion': ";
+    cin >> numbers;
+    Print(numbers);
+    return 0;
 }
-
-
