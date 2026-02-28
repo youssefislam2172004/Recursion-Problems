528158245fc3dfca9e52389eb1da07628e168e3a
YoussefIslam217 <youssef23012591@gmail.com>
2026-02-28 22:27:35 +0200

    Sevevnth Commit

--- Recursion-Problems/Recursion-Problems.cpp
+++ Recursion-Problems/Recursion-Problems.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
+//G. Pyramid
+void Pyramid(int w, int n)
+{
+	if (n == 0)
+		return;
+	else
+	{
+		for (int i = 0; i < n; i++)
+			cout << " ";
+		for (int i = 0; i < w; i++)
+			cout << "*";
+	}
+	cout << endl;
+	Pyramid(w + 2, n - 1);
+}
 
 int main()
 {
-    cout << "Ramadan Kareem, Eng.Mahmoud Hammad";
+	int width = 1, NumberOfLines;
+	cout << "Enter the number of lines: ";
+	cin >> NumberOfLines;
+	Pyramid(width, NumberOfLines);
+	return 0;
 }
-
 
