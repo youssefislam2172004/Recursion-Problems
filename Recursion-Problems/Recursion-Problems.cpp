5d67dea0a77cb5ac65b871da4d108a83052a371b
YoussefIslam217 <youssef23012591@gmail.com>
2026-02-28 22:04:44 +0200

    Third-Commit

--- Recursion-Problems/Recursion-Problems.cpp
+++ Recursion-Problems/Recursion-Problems.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 using namespace std;
+//Problem-C. Print from N to 1
+void PrintFromNTo1(int n)
+{
+    if (n == 0)
+        return;
+    else
+        cout << n << " ";
+    PrintFromNTo1(n - 1);
+}
 
 int main()
 {
-    cout << "Ramadan Kareem, Eng.Mahmoud Hammad";
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    cout << "Numbers from " << n << " to 1: ";
+    PrintFromNTo1(n);
+    return 0;
 }
-
 
