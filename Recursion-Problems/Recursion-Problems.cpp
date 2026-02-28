4f2811e0b4d99f61c54c1f85a644d8ef987dad11
YoussefIslam217 <youssef23012591@gmail.com>
2026-02-28 22:10:50 +0200

    Fourth-Commit

--- Recursion-Problems/Recursion-Problems.cpp
+++ Recursion-Problems/Recursion-Problems.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
+//Problem-D. Print Digits using Recursion
+void PrintDigits(int n)
+{
+	if (n == 0)
+		return;
+	PrintDigits(n / 10);
+	cout << n % 10 << " ";
+}
 
 int main()
 {
-    cout << "Ramadan Kareem, Eng.Mahmoud Hammad";
+	int number;
+	cout << "Enter a number: ";
+	cin >> number;
+	cout << "Digits of the number: ";
+	PrintDigits(number);
+	return 0;
 }
-
-
